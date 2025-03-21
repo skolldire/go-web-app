db7cd6067769dcd3e0804abf5d1268d7a22806a2
Fredy Hernández <hfredy717@outlook.com>
2025-03-21 11:38:58 -0500

    :construction: initial version

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 
 REPO_NAME=$(basename $(git rev-parse --show-toplevel) 2>/dev/null || basename $(pwd))
 
-echo "Inicializando Go Module con el nombre: github.com/usuario/$REPO_NAME"
+echo "Inicializando Go Module con el nombre: github.com/skolldire/$REPO_NAME"
 
 go mod init "github.com/skolldire/$REPO_NAME"
 go mod tidy
